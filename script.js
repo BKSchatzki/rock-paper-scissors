4cdc90efce72f39c9c91731ce37e186ea25b0426
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2022-12-05 15:40:56 -0800

    Add comment of basic code structure to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// declare function getComputerChoice() to randomly return Rock, Paper, or Scissors
+
+
+
+
+// write function playRound() to play single round using playerSelection and computerSelection and return a string showing the result
+
+
+
+
+// write function called game(), calling playRound() inside of it and counting how many rounds played, as well as reporting on round victories and match victories
