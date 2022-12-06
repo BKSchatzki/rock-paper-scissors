af9b41d9578ca3ada58b1fbef6354eefb67c160c
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2022-12-06 13:14:38 -0800

    Add if else statement to return RPS and assign to computerSelection

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let compuerSelection;
 let playerSelection;
 
 // declare function getComputerChoice() to randomly return Rock, Paper, or Scissors
@@ -7,12 +6,19 @@ function getComputerChoice() {
     // multiply result by 3
     // round number down to integer, giving three possible values: 0, 1, and 2
     let randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
     // use if statements to return "Rock" for 0, "Paper" 1, and "Scissors" for 2
+    if (randomNumber === 0) {
+        return "Rock";
+    }   else if (randomNumber === 1) {
+        return "Paper";
+    }   else {
+        return "Scissors";
+    }
     // log console for testing
 }
-    
-getComputerChoice()
+
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
 
 
 // write function playRound() to play single round using playerSelection and computerSelection and return a string showing the result
