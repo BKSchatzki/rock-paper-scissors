1f0ee8dbca19d32425c2fccc3b2a7decfef4eee2
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2022-12-06 13:07:01 -0800

    Add getComputerChoice

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function getComputerChoice() {
     // select random number between 0 and 1
     // multiply result by 3
     // round number down to integer, giving three possible values: 0, 1, and 2
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
     // use if statements to return "Rock" for 0, "Paper" 1, and "Scissors" for 2
     // log console for testing
 }
     
-
+getComputerChoice()
 
 
 // write function playRound() to play single round using playerSelection and computerSelection and return a string showing the result
