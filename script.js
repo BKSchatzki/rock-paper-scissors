8541858787a26d1c58e0f06f52ef405810d4fa5c
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2022-12-07 11:43:41 -0800

    Place selection variables and playRound inside for loop to 5

--- script.js
+++ script.js
@@ -37,38 +37,42 @@ function getPlayerChoice() {
         }
     }
     
-// initialize variable computerSelection with value of function getComputerChoice
-    // log to console for testing (will not be in final game)
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-    
-// initialize variable playerSelection with value of function getPlayerChoice
-    // log to console for testing (will not be in final game)
-let playerSelection = getPlayerChoice();
-console.log(playerSelection);
+for (let round = 0;  round < 5; round++) {
 
-// compare variable playerSelection to variable computerSelection to determine winner of round
-    // if player canceled prompt, say player aborted the game
-    // if variables are the same, say it's a tie and what was picked
-    // if player wins, say so and what was picked
-    // if computer wins, say so and what was picked
-function playRound() {
-    if (playerSelection === null) {
-        console.log("You aborted!");
-    } else if (computerSelection === playerSelection) {
-        console.log(`It's a tie! You both picked ${playerSelection}!`);
-    } else if (
-        computerSelection === "Rock" && playerSelection === "Paper" ||
-        computerSelection === "Paper" && playerSelection === "Scissors" ||
-        computerSelection === "Scissors" && playerSelection === "Rock") {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-    } else if ( 
-        playerSelection === "Rock" && computerSelection === "Paper" ||
-        playerSelection === "Paper" && computerSelection === "Scissors" ||
-        playerSelection === "Scissors" && computerSelection === "Rock") {
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+    // initialize variable computerSelection with value of function getComputerChoice
+        // log to console for testing (will not be in final game)
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+        
+    // initialize variable playerSelection with value of function getPlayerChoice
+        // log to console for testing (will not be in final game)
+    let playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+
+    // compare variable playerSelection to variable computerSelection to determine winner of round
+        // if player canceled prompt, say player aborted the game
+        // if variables are the same, say it's a tie and what was picked
+        // if player wins, say so and what was picked
+        // if computer wins, say so and what was picked
+    function playRound() {
+        if (playerSelection === null) {
+            console.log("You aborted!");
+        } else if (computerSelection === playerSelection) {
+            console.log(`It's a tie! You both picked ${playerSelection}!`);
+        } else if (
+            computerSelection === "Rock" && playerSelection === "Paper" ||
+            computerSelection === "Paper" && playerSelection === "Scissors" ||
+            computerSelection === "Scissors" && playerSelection === "Rock") {
+                console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+        } else if ( 
+            playerSelection === "Rock" && computerSelection === "Paper" ||
+            playerSelection === "Paper" && computerSelection === "Scissors" ||
+            playerSelection === "Scissors" && computerSelection === "Rock") {
+                console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+        }
     }
+    
+    // log to console for testing
+    console.log(playRound());
+    
 }
-
-// log to console for testing
-console.log(playRound());
