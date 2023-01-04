92a914323ca4860767a36bc27a0666fb75ba2b5b
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-01-04 14:29:09 -0800

    Move playRound declaration out and event listener into loop

--- script.js
+++ script.js
@@ -9,29 +9,37 @@ function getComputerChoice() {
     }
 }
 
+function playRound (playerSelection) {
+    computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        document.getElementById("result").innerText =`It's a tie. You both picked ${playerSelection}.`;
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Rock")) {
+        document.getElementById("result").innerText = `You win. ${playerSelection} beats ${computerSelection}.`;
+        playerScore++;
+        document.getElementById("playerScore").innerText = playerScore;
+    } else {
+        document.getElementById("result").innerText = `You lose. ${computerSelection} beats ${playerSelection}.`;
+        computerScore++;
+        document.getElementById("computerScore").innerText = computerScore;
+    }
+}
+
 let playerScore = 0;
 let computerScore = 0;
 
 while (playerScore < 5 && computerScore < 5) {
     
-    function playRound (playerSelection) {
-        computerSelection = getComputerChoice();
-
-        if (playerSelection === computerSelection) {
-            document.getElementById("result").innerText =`It's a tie. You both picked ${playerSelection}.`;
-        } else if (
-            (playerSelection === "Rock" && computerSelection === "Scissors") ||
-            (playerSelection === "Scissors" && computerSelection === "Paper") ||
-            (playerSelection === "Paper" && computerSelection === "Rock")) {
-            document.getElementById("result").innerText = `You win. ${playerSelection} beats ${computerSelection}.`;
-            playerScore++;
-            document.getElementById("playerScore").innerText = playerScore;
-        } else {
-            document.getElementById("result").innerText = `You lose. ${computerSelection} beats ${playerSelection}.`;
-            computerScore++;
-            document.getElementById("computerScore").innerText = computerScore;
-        }
-    }
+    const rockBtn = document.getElementById("rock");
+    const paperBtn = document.getElementById("paper");
+    const scissorsBtn = document.getElementById("scissors");
+    
+    rockBtn.addEventListener("click", () => playRound("Rock"));
+    paperBtn.addEventListener("click", () => playRound("Paper"));
+    scissorsBtn.addEventListener("click", () => playRound("Scissors"));
     
     if (playerScore === 5) {
         document.getElementById("result").innerText = "First to 5! You win!";
@@ -39,13 +47,5 @@ while (playerScore < 5 && computerScore < 5) {
     } else if (computerScore === 5) {
         document.getElementById("result").innerText = "Beaten to 5! You lose.";
         break;
-    } else break;
+    } else break
 }
-
-const rockBtn = document.getElementById("rock");
-const paperBtn = document.getElementById("paper");
-const scissorsBtn = document.getElementById("scissors");
-
-rockBtn.addEventListener("click", () => playRound("Rock"));
-paperBtn.addEventListener("click", () => playRound("Paper"));
-scissorsBtn.addEventListener("click", () => playRound("Scissors"));
