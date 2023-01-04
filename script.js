a145755b93c0f3587a7d815da0854a3d85a0897a
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-01-04 13:29:37 -0800

    Re-add event listeners

--- script.js
+++ script.js
@@ -8,28 +8,11 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-let computerSelection = getComputerChoice();
 
-function playRound(playerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log(`It's a tie! You both picked ${playerSelection}! No points awarded this round.`);
-    } else if (
-        computerSelection === "Rock" && playerSelection === "Paper" ||
-        computerSelection === "Paper" && playerSelection === "Scissors" ||
-        computerSelection === "Scissors" && playerSelection === "Rock") {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-    } else if ( 
-        playerSelection === "Rock" && computerSelection === "Paper" ||
-        playerSelection === "Paper" && computerSelection === "Scissors" ||
-        playerSelection === "Scissors" && computerSelection === "Rock") {
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
-    }
-}
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
 
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-
-rock.addEventListener("click", playRound("Rock"));
-paper.addEventListener("click", playRound("Paper"));
-scissors.addEventListener("click", playRound("Scissors"));
+rockBtn.addEventListener("click", () => console.log("Rock"));
+paperBtn.addEventListener("click", () => console.log("Paper"));
+scissorsBtn.addEventListener("click", () => console.log("Scissors"));
