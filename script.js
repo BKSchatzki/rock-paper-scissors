ef948f17ea477aa71da843f6d517214ded076555
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-01-04 13:25:24 -0800

    Delete EVERYTHING except getComputerChoice

--- script.js
+++ script.js
@@ -8,44 +8,28 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-
-rock.addEventListener("click", () => {return "Rock"; });
-paper.addEventListener("click", () => {return "Paper"; });
-scissors.addEventListener("click", () => {return "Scissors"; });
-
-let computerScore;
-let playerScore;
-
-computerScore = 0;
-playerScore = 0;
-
 let computerSelection = getComputerChoice();
-    
-let playerSelection = getPlayerChoice();
 
-function playRound() {
-    if (playerSelection === null) {
-        console.log("You aborted the round! The computer gets a point!");
-        computerScore += 1;
-    } else if (computerSelection === playerSelection) {
+function playRound(playerSelection) {
+    if (playerSelection === computerSelection) {
         console.log(`It's a tie! You both picked ${playerSelection}! No points awarded this round.`);
     } else if (
         computerSelection === "Rock" && playerSelection === "Paper" ||
         computerSelection === "Paper" && playerSelection === "Scissors" ||
         computerSelection === "Scissors" && playerSelection === "Rock") {
             console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-            playerScore += 1;
     } else if ( 
         playerSelection === "Rock" && computerSelection === "Paper" ||
         playerSelection === "Paper" && computerSelection === "Scissors" ||
         playerSelection === "Scissors" && computerSelection === "Rock") {
             console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
-            computerScore += 1;
     }
 }
 
-playRound();
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+rock.addEventListener("click", playRound("Rock"));
+paper.addEventListener("click", playRound("Paper"));
+scissors.addEventListener("click", playRound("Scissors"));
