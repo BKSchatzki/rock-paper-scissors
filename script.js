65d9414ae8976305069cf026d7007afccebe99ca
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-01-04 17:00:20 -0800

    Delete score counter code

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
+let playerWinRound;
+let computerWinRound;
+
 function playRound (playerSelection) {
     computerSelection = getComputerChoice();
-    let playerWinRound;
-    let computerWinRound;
 
     if (playerSelection === computerSelection) {
         document.getElementById("result").innerText =`It's a tie. You both picked ${playerSelection}.`;
@@ -24,10 +25,10 @@ function playRound (playerSelection) {
         (playerSelection === "Scissors" && computerSelection === "Paper") ||
         (playerSelection === "Paper" && computerSelection === "Rock")) {
         document.getElementById("result").innerText = `You win. ${playerSelection} beats ${computerSelection}.`;
-        playerWinRound = true;
+        return playerWinRound = true;
     } else {
         document.getElementById("result").innerText = `You lose. ${computerSelection} beats ${playerSelection}.`;
-        computerWinRound = true;
+        return computerWinRound = true;
     }
 }
 
@@ -38,21 +39,3 @@ const scissorsBtn = document.getElementById("scissors");
 rockBtn.addEventListener("click", () => playRound("Rock"));
 paperBtn.addEventListener("click", () => playRound("Paper"));
 scissorsBtn.addEventListener("click", () => playRound("Scissors"));
-
-if (playerWinRound) {
-    playerScore++;
-    document.getElementById("playerScore").innerText = playerScore;
-}
-
-if (computerWinRound) {
-    computerScore++;
-    document.getElementById("computerScore").innerText = computerScore;
-}
-
-if (playerScore === 5) {
-    document.getElementById("result").innerText = "First to 5! You win!";
-    break;
-} else if (computerScore === 5) {
-    document.getElementById("result").innerText = "Beaten to 5! You lose.";
-    break;
-}
